ab77ea55ac1a35a8ba4fe576518fd9d7b3db6435
aidenpantzer <aidenbpantzer@gmail.com>
2024-12-10 22:34:37 -0500

    Added comp code after (almost) losing it, simplified code, added basic automation functions to test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMain.java
@@ -20,13 +20,14 @@ import java.lang.Math;
 @TeleOp(name="[Current] 4234 Main TeleOP", group="Linear OpMode") // Set the program name
 
 // Initialization Code
-public class MainFTC extends LinearOpMode {
+public class TeleOpMain extends LinearOpMode {
 
     // Initialize Servos
     public Servo wrist1;
     public Servo wrist2;
     public CRServo intake1;
     public CRServo intake2;
+    public Servo winch;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -37,16 +38,24 @@ public class MainFTC extends LinearOpMode {
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRight");
         DcMotor backRightMotor = hardwareMap.dcMotor.get("backRight");
         DcMotor armExtend = hardwareMap.dcMotor.get("armExtend");
+        armExtend.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armExtend.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         DcMotor armLift = hardwareMap.dcMotor.get("armLift");
+        armLift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armLift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        DcMotor winchMotor = hardwareMap.dcMotor.get("winchs");
         frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         armLift.setDirection(DcMotorSimple.Direction.REVERSE);
+        armExtend.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
 
         // Setup Servos
         wrist1 = hardwareMap.get(Servo.class, "wrist1"); // Setup servos
         wrist2 = hardwareMap.get(Servo.class, "wrist2");
         intake1 = hardwareMap.get(CRServo.class, "intake1");
         intake2 = hardwareMap.get(CRServo.class, "intake2");
+        winch = hardwareMap.get(Servo.class, "winch");
 
         // Setup IMU
         IMU imu = hardwareMap.get(IMU.class, "imu"); // This allows for field centric
@@ -55,21 +64,30 @@ public class MainFTC extends LinearOpMode {
                 RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD)); //Orientation of the control hub
         imu.initialize(parameters);
 
+
         // Initialize Variables
         double debugInfo = 0; // Provides state info for the debug screen, in numeric form. State values and their meanings can be found towards the end of the code, along with other telemetry data
-        double driveModifier = 1; // Slow mode. 1 = full speed, 0.5 = half speed, 0.25 = quarter speed, ect.
         double armLiftMin = -100; // Minimum lift value of the arm
-        double armLiftMax = 5000; // Maximum lift value of the arm
+        double armLiftMax = 4050; // Maximum lift value of the arm
         double armExtendMax = 5020; // Maximum extension value of the arm
         double automation = 0; // Current automation
         double automationStep = 0; // Current automation step
         boolean fieldCentricActive = true;
-        double frontLeftPower = 0;
-        double backLeftPower = 0;
-        double frontRightPower = 0;
-        double backRightPower = 0;
-        double denominator = 1;
-        //Debug Information
+        double frontLeftPower;
+        double backLeftPower;
+        double frontRightPower;
+        double backRightPower;
+        double denominator;
+        double armExtendMax42;
+        winchMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Targets
+        int pickUpExtentionTarget = 500;
+        int pickUpLiftTarget = 200;
+
+
+
+        // Debug Information
         telemetry.addData("Status", "Initializing - Waiting for Start"); //Log status info
         telemetry.update(); // Push status to driver station
 
@@ -85,15 +103,16 @@ public class MainFTC extends LinearOpMode {
             } else if (gamepad1.right_stick_button) {
                 fieldCentricActive = false;
             }
-            double xP = -gamepad1.left_stick_y;
-            double yP = gamepad1.left_stick_x;
-            double rP = gamepad1.right_stick_x;
-            double slowMode = gamepad1.right_bumper ? .4 : 1;
+            double xP = gamepad1.left_stick_x;
+            double yP = -gamepad1.left_stick_y;
+            double rP = -gamepad1.right_stick_x;
+            double driveModifier = gamepad1.right_bumper ? .4 : 1; // Slow mode. 1 = full speed, 0.5 = half speed, 0.25 = quarter speed, ect.
             double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
             double rotX = xP * Math.cos(-botHeading) - yP * Math.sin(-botHeading);
             double rotY = xP * Math.sin(-botHeading) + yP * Math.cos(-botHeading);
             rotX = rotX * 1.1;
-            if (fieldCentricActive == true) {
+
+            if (fieldCentricActive) {
                 denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rP), 1);
                 frontLeftPower      =     (rotY + rotX + rP) / denominator;
                 backLeftPower       =     (rotY - rotX + rP) / denominator;
@@ -110,47 +129,61 @@ public class MainFTC extends LinearOpMode {
 
             //Arm and Intake Inputs
             double armExtendPower = -gamepad2.left_stick_y; // Position inputs
-            double armLiftPower = gamepad2.right_stick_y;
+            double armLiftPower = -gamepad2.right_stick_y;
             double armExtendPosition = armExtend.getCurrentPosition();
             double armLiftPosition = armLift.getCurrentPosition();
             double armLiftRotation = (armLiftPosition / 5000.0);
-            double armExtendMax42 = (Math.cos((armLiftRotation) * (Math.PI / 2)) <= 0.01) ? Double.POSITIVE_INFINITY : 3000 / Math.cos((armLiftPosition / 5000.0) * (Math.PI / 2)); // Calculates the arm extension limit to prevent teh arm exceeding the 42" limit
+            if (armLiftRotation < 4000) {
+                armExtendMax42 = (Math.cos((armLiftRotation) * (Math.PI / 2)) <= 0.01) ? Double.POSITIVE_INFINITY : 3000 / Math.cos((armLiftPosition / 5000.0) * (Math.PI / 2)); // Calculates the arm extension limit to prevent the arm exceeding the 42" limit
+            } else {
+                armExtendMax42 = (Double.POSITIVE_INFINITY);
+            }
             double armExtendPowerOffset = 1 - ((armExtendMax42 - armExtendPosition) / 250);
             double armLiftPowerOffset =  1 - ((armExtendMax42 - armExtendPosition) / 125);
+            double autoDriveModifier = (armExtendPosition > 3500 ? Math.max((5500 - armExtendPosition) / 2000, .4) : 1);
 
-
-
-            //Automation Inputs
-            if (gamepad2.y && automation != 0) {
-                automation = 0;
-                debugInfo = 3;
-            } else if (automation == 0) {
-                if (gamepad2.a) {
-                    automation = 1;
-                    debugInfo = 2;
-                } else if (gamepad2.b) {
-                    automation = 2;
-                    automationStep = 1;
-                    debugInfo = 2;
-                } else if (gamepad2.x) {
-                    automation = 3;
-                    automationStep = 1;
-                    debugInfo = 2;
-                }
-            }
+//            //Automation Inputs
+//            if (gamepad2.y && automation != 0) {
+//                automation = 0;
+//                debugInfo = 3;
+//            } else if (automation == 0) {
+//                if (gamepad2.x) {
+//                    automation = 1;
+//                    debugInfo = 2;
+//                } else if (gamepad2.a) {
+//                    automation = 2;
+//                    automationStep = 1;
+//                    debugInfo = 2;
+//                } else if (gamepad2.b) {
+//                    automation = 3;
+//                    automationStep = 1;
+//                    debugInfo = 2;
+//                }
+//            }
 
             // Reset IMU
-            if (gamepad1.start || gamepad2.start) {
+            if (gamepad1.options) {
                 imu.resetYaw();
             }
 
             // Drive Power
-            frontLeftMotor.setPower(frontLeftPower * slowMode);
-            backLeftMotor.setPower(backLeftPower * slowMode);
-            frontRightMotor.setPower(frontRightPower * slowMode);
-            backRightMotor.setPower(backRightPower * slowMode);
+            frontLeftMotor.setPower(frontLeftPower * driveModifier);
+            backLeftMotor.setPower(backLeftPower * driveModifier);
+            frontRightMotor.setPower(frontRightPower * driveModifier);
+            backRightMotor.setPower(backRightPower * driveModifier);
 
-
+            // Auto Braking
+            if (armExtendPosition > 3500) {
+                frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+                frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+                backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+                backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+            } else {
+                frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            }
 
             // Arm Power
             if (automation == 0) { // == Driver controlled
@@ -158,7 +191,7 @@ public class MainFTC extends LinearOpMode {
                 // This subregion contains the code for the arm extension while in the driver controlled mode
                 //region Arm Extend
                 if (armExtendPosition > 150) { // Prevents arm hitting lower limit
-                    if (armLiftPosition > 4500 || armExtendPosition + 250 > armExtendMax42) { // Is arm extended too far?
+                    if (armExtendPosition + 250 > armExtendMax || armExtendPosition + 250 > armExtendMax42) { // Is arm extended too far?
                         if (armExtendPosition + 250 > armExtendMax42 && armExtendPosition > 2500) { //Is arm at risk of hitting the horizontal expansion limit?
                             armExtend.setPower(armExtendPower - armExtendPowerOffset);
                         } else if (armExtendPosition > armExtendMax) {
@@ -195,31 +228,27 @@ public class MainFTC extends LinearOpMode {
 
                 // This subregion contains the code for the arm lifting while in the driver controlled state
                 // region Arm Lift
-                if (armLiftPower > 0 && armExtendPosition < armLiftMax) { // Is arm lowering and above limit
-                    if (Math.abs(armExtendPosition - armExtendMax42) < 125) {
-                        armLift.setPower(armLiftPower + armLiftPowerOffset); // Is arm lowering too fast?
-                    } else {
-                        armLift.setPower(armLiftPower);
-                    }
-                }
-                else if (armLiftPower < 0 && armLiftPosition > armLiftMin) { // Is arm rising and below limit?
+                if (armLiftPower > 0 && armLiftPosition < armLiftMax) { // Is arm rising and below limit?
+                    armLift.setPower(armLiftPower); // Is arm lowering too fast?
+                } else if (armLiftPower < 0 && armLiftPosition > armLiftMin) { // Is arm lowering and above limit
                     armLift.setPower(armLiftPower);
-                }
-                else { // Stop the arm
+                } else { // Stop the arm
                     armLift.setPower(0);
                 }
                 //endregion
             } else if (automation == 1) {
-                if (Math.abs(armExtendPosition - 1000) > 5) {
-                    armExtend.setPower((1000 - armExtendPosition)/400);
-                } else {
+                armExtend.setTargetPosition(pickUpExtentionTarget);
+                armExtend.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                armExtend.setPower(1);
+                if (Math.abs (armExtend.getCurrentPosition() - pickUpExtentionTarget) < 30) {
+                    armExtend.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
                     automation = 0;
-                    debugInfo = 4;
-                    armExtend.setPower(0);
                 }
                 //endregion
             } else if (automation == 2) {
-                //Do nothing (FOR NOW!!)
+
+                sleep(0);
+
             } else if (automation == 3) {
                 if (automationStep == 1) {
                     if (Math.abs(armExtendPosition - 500) < 50) {
@@ -243,53 +272,76 @@ public class MainFTC extends LinearOpMode {
             //Servos
 
             // region Wrist
-            if (gamepad2.dpad_down) {
-                wrist1.setPosition(.7);
-                wrist2.setPosition(.3);
-            } else if (gamepad2.dpad_up) {
-                wrist1.setPosition(0);
-                wrist2.setPosition(1);
-            } else if (gamepad2.dpad_left || gamepad2.dpad_right) {
-                wrist1.setPosition(.5);
-                wrist2.setPosition(.5);
+            if (automation == 0) {
+                if (gamepad2.dpad_down) {
+                    wrist1.setPosition(.75);
+                    wrist2.setPosition(.25);
+                } else if (gamepad2.dpad_up && armLiftPosition < 3500) {
+                    wrist1.setPosition(0);
+                    wrist2.setPosition(1);
+                } else if (gamepad2.dpad_left || gamepad2.dpad_right || ((wrist1.getPosition() == 0) && (armLiftPosition > 3500))) {
+                    wrist1.setPosition(.5);
+                    wrist2.setPosition(.5);
+                }
+            } else if (automation == 1) {
+
             }
             // endregion Wrist
 
             // region Intake
-            if (gamepad2.left_bumper) { //Outtake
-                intake1.setPower(.5);
-                intake2.setPower(-.5);
-            } else if (gamepad2.left_trigger > .2) { //Intake
+            if (automation == 0) {
+                if (gamepad2.left_bumper) { //Outtake
+                    intake1.setPower(.3);
+                    intake2.setPower(-.3);
+                } else if (gamepad2.left_trigger > .2) { //Intake
+                    intake1.setPower(-.75);
+                    intake2.setPower(.75);
+                } else {
+                    intake1.setPower(0);
+                    intake2.setPower(0);
+                }
+            } else if (automation == 1) {
                 intake1.setPower(-.5);
                 intake2.setPower(.5);
-            } else {
-                intake1.setPower(0);
-                intake2.setPower(0);
             }
             // endregion Intake
 
             //Winch Power
+            if (gamepad1.dpad_up) {
+                winch.setPosition(1);
+            } else if (gamepad1.dpad_down){
+                winch.setPosition(0);
+            }
+            if (gamepad1.dpad_left) {
+                winchMotor.setPower(-1);
+            } else if (gamepad1.dpad_right) {
+                winchMotor.setPower(1);
+            } else {
+                winchMotor.setPower(0);
+            }
 
             //Debug Information
             if (debugInfo == 0) {
-                telemetry.addData("Status", "Initializing - Waiting for Start");
+                telemetry.addData("Status", "Waiting for Start");
                 debugInfo = 1;
             } else if (debugInfo == 1) {
                 telemetry.addData("Status", "Running - Driver Controlled");
             } else if (debugInfo == 2) {
-                telemetry.addData("Status", "Running - Automation Active. Press [Y] on Controller 2 to Cancel");
+                telemetry.addData("Status", "Auto Active. Press [Y] to cancel");
             } else if (debugInfo == 3) {
-                telemetry.addData("Status", "Running - Driver Controlled; Automation Cancelled Manually");
+                telemetry.addData("Status", "Auto Cancelled");
             } else if (debugInfo == 4) {
-                telemetry.addData("Status", "Running - Driver Controlled; Automation Finished Successfully");
+                telemetry.addData("Status", "Auto Completed");
             } else if (debugInfo == 5) {
-                telemetry.addData("Status", "Unknown - Unspecified Error");
+                telemetry.addData("Status", "Uh Oh");
             }
             telemetry.addData("Arm Extension", armExtendPosition);
             telemetry.addData("Arm Extension %", armExtendPosition / armExtendMax);
             telemetry.addData("Automation", automationStep);
             telemetry.addData("armExtendMax42", armExtendMax42);
-            telemetry.addData("aepo", armExtendPowerOffset);
+            telemetry.addData("armExtendPowerOffset", armExtendPowerOffset);
+            telemetry.addData("autoDriveModifier", autoDriveModifier);
+            telemetry.addData("alp", armLiftPosition);
             telemetry.update();
         }
     }
