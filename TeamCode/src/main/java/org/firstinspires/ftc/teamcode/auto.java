9270ad7424c32668360480793a0a65a07867d4ac
i am probably Ethan <ethanpantzer@gmail.com>
2025-01-11 08:41:57 -0500

    e

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto.java
@@ -8,10 +8,14 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IMU;
 
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+import java.lang.Math;
 
 //intake1+ = outtake
-@Autonomous(name="12/12/2024", group="Linear OpMode")
-public class BasicAutoCentric2 extends LinearOpMode {
+@Autonomous(name="124", group="Linear OpMode")
+public class auto extends LinearOpMode {
 
 
     //Setup IMU
@@ -25,6 +29,16 @@ public class BasicAutoCentric2 extends LinearOpMode {
     private DcMotor backRight;
     private DcMotor shortArm;
     private DcMotor longArm;
+    public Servo wrist1;
+    public Servo wrist2;
+    public CRServo intake1;
+    public CRServo intake2;
+    public Servo winch;
+    public Servo specimen;
+    private DcMotor armExtend;
+    private DcMotor armLift;
+    double armState = 1;
+
     //private CRServo intake1;
     //private CRServo intake2;
     //armExtend.setDirection(DcMotorSimple.Direction.REVERSE); //Might not be needed; test on Tuesday
@@ -46,7 +60,18 @@ public class BasicAutoCentric2 extends LinearOpMode {
 
     }
 
+    public void armTO (int liftPosTo, int extendPosTo, float speed, float wrist) {
 
+        armLift.setTargetPosition(liftPosTo);
+        armLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armExtend.setTargetPosition(extendPosTo);
+        armExtend.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armLift.setPower(speed * 1.25 / 10);
+        armExtend.setPower(speed / 10);
+        armState = -1; // Automated action in progress
+        wrist1.setPosition(1    - (wrist / 10));
+        wrist2.setPosition(wrist / 10);
+    }
     public void drive(double speed, double distance) {
         frontLeft.setPower(speed);
         backLeft.setPower(speed);
@@ -57,15 +82,8 @@ public class BasicAutoCentric2 extends LinearOpMode {
         }
         stopDrive();
     }
-    public void correct(double speed, double distance) {
-        frontLeft.setPower(speed);
-        backLeft.setPower(speed);
-        while (Math.abs(frontLeft.getCurrentPosition())<distance && opModeIsActive()) {
-            sleep(1);
-        }
-        stopDrive();
-    }
-           public void driveLeft(double speed, double distance) {
+
+    public void driveLeft(double speed, double distance) {
         frontLeft.setPower(0-speed);
         backLeft.setPower(speed);
         frontRight.setPower(speed);
@@ -75,16 +93,39 @@ public class BasicAutoCentric2 extends LinearOpMode {
         }
         stopDrive();
     }
+
+    public void arm(double speed, double distance) {
+        frontLeft.setPower(speed);
+        backLeft.setPower(speed);
+        while (Math.abs(frontLeft.getCurrentPosition())<distance && opModeIsActive()) {
+            sleep(1);
+        }
+        stopDrive();
+    }
+
+    public void correct(double speed, double distance) {
+        frontLeft.setPower(speed);
+        backLeft.setPower(speed);
+        while (Math.abs(frontLeft.getCurrentPosition())<distance && opModeIsActive()) {
+            sleep(1);
+        }
+        stopDrive();
+    }
+
     @Override
     public void runOpMode() throws InterruptedException {
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
         backLeft = hardwareMap.get(DcMotor.class, "backLeft");
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         backRight = hardwareMap.get(DcMotor.class, "backRight");
-       // intake1 = hardwareMap.get(CRServo.class, "intake1");
-       // intake2 = hardwareMap.get(CRServo.class, "intake2");
-       //shortArm = hardwareMap.get(DcMotor.class, "shortArm");
-       //longArm = hardwareMap.get(DcMotor.class, "longArm");
+        armLift = hardwareMap.get(DcMotor.class, "armLift");
+        armExtend = hardwareMap.get(DcMotor.class, "armExtend");
+        wrist1 = hardwareMap.get(Servo.class, "wrist1");
+        wrist2 = hardwareMap.get(Servo.class, "wrist2");
+        intake1 = hardwareMap.get(CRServo.class, "intake1");
+        intake2 = hardwareMap.get(CRServo.class, "intake2");
+        winch = hardwareMap.get(Servo.class, "winch");
+        specimen = hardwareMap.get(Servo.class, "specimen");
 
         frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         backRight.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -105,6 +146,9 @@ public class BasicAutoCentric2 extends LinearOpMode {
         //double ticksPerInch = 41.7; // TEST/BOT based on wheel sizes and motor rpm
         double driveSpeed = 0.3;    // can be changed
         stopDrive();
+        drive(-driveSpeed, 5 * ticksPerInch);
+
+
         driveLeft(-driveSpeed * reversed, 9* ticksPerInch);
 
         drive(driveSpeed, 50* ticksPerInch);
@@ -115,10 +159,9 @@ public class BasicAutoCentric2 extends LinearOpMode {
         driveLeft(-driveSpeed * reversed, 14* ticksPerInch);
         drive(-driveSpeed, 46* ticksPerInch);
         correct(-driveSpeed, 0);
-
-        drive(driveSpeed, 48* ticksPerInch);
-        driveLeft(-driveSpeed * reversed, 18* ticksPerInch);
-        drive(-driveSpeed, 48* ticksPerInch);
+//        drive(driveSpeed, 48* ticksPerInch);
+  //      driveLeft(-driveSpeed * reversed, 18* ticksPerInch);
+    //    drive(-driveSpeed, 48* ticksPerInch);
 
         //Intake()
     //rotate(-130);
